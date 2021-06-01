461ef743cd13e2c0a3bb92b35b75b1d2ea73318f
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2021-06-01 15:48:59 +0300

    Done exercise: tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  let cels = parseFloat(((fahr - 32) * 5/9).toFixed(1));
+  return cels;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let fahr = parseFloat(((cels * 9/5) + 32).toFixed(1));
+  return fahr;
 };
 
 module.exports = {
