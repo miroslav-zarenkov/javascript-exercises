d224f8c032ae82a3d2b212deeb82e5d030879509
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2021-06-01 15:07:18 +0300

    Done exercise: sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (x < 0 || y < 0 || typeof x !== 'number' || typeof y !== 'number'){
+        return 'ERROR';
+    }
+    if(y < x){
+        let a = x;
+        x = y;
+        y = a;
+    }
+    let sum = ((y - x + 1) * (x + y)) / 2;
+    return sum;
 };
 
 module.exports = sumAll;
