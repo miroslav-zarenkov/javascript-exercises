cee52d53901e3e7d9949a206dba787c643149916
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2021-06-01 14:12:54 +0300

    Done exercise: removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...removeValues) {
+    for (const removeValue of removeValues){
+        if (myArray.indexOf(removeValue) !==-1){
+            myArray.splice(myArray.indexOf(removeValue), 1);
+        }
+    }
+    return myArray;
 };
 
 module.exports = removeFromArray;
