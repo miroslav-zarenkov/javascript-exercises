0eb56002de80878ad7961f71be4803fd5f87e131
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-09-09 18:48:47 +0300

    Done exercise: calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return a ? a * factorial(a - 1) : 1;
 };
 
 module.exports = {
