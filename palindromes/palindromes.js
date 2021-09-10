74ba1eabc262fe2aff268aede5a3a85f013da0cd
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-09-10 15:43:28 +0300

    Done exercise: palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    if (string.toLowerCase().split('').reverse().join('')
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "") === 
+    string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "")) {
+        return true
+    }
+        return false
+}
 
 module.exports = palindromes;
+
+
+//string.replace(/\s/g,"") delete spaces
+//string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") delete all punctuation
+//string.toLowerCase() lower case
+//string.reverse() reverse not works on string only on arrays
+//string.split('').reverse().join('') split to array, reverse, join back to string
