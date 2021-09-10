58873ec92e24feb3cc2250722f5e377594d7de7f
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-09-10 17:49:43 +0300

    Done exercise getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+   let newArray = array.map(function(key){
+        return key.title;
+    })
+    return newArray;
 };
 
 module.exports = getTheTitles;
