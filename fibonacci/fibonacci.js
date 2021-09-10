fa0c5a05d2d0f39885b3f385cf059f3f0ceaa4e2
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-09-10 17:13:32 +0300

    Done exercise: fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0){
+        return "OOPS";
+    }
+    else{
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++){
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
+}
 
 module.exports = fibonacci;
