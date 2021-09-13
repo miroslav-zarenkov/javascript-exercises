be1438f42f84a6af2fca08c2fe472be6b77a4e5c
Miroslav Zarenkov <miroslav@MBP-Miroslav.Dlink>
2021-09-13 17:49:04 +0300

    Done exercise: findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+      let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+      let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      if (oldestAge < currentAge){
+          return currentPerson;
+      } else{
+          return oldest;
+      }
+    })
+  };
+  
+  let getAge = function(birthYear, deathYear) {
+    if (!deathYear) {
+      deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+  };
 
 module.exports = findTheOldest;
